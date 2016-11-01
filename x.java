e2b09b3b6b97480f0d3fb8048fb4521df281473e
Grzesiek <elrok123@gmail.com>
2016-11-01 17:03:26 +0100

    KONIEC NA DZISIAJ

--- x.java
+++ x.java
@@ -5,7 +5,7 @@ public class x
 public static void main (String args[])
 {
 System.out.println("Jak masz na imię? ")
-System.out.println("Patka ")
-
+System.out.println("Patka ");
+System.out.println("Koniec");
 }
 }
